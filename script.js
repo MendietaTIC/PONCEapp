0dc2d8ddf861d42e251d24f8e38882763a91e6e4
Glitch (glitch-hello-website) <none>
2024-08-30 21:45:52 +0000

    🍽🕶 Checkpoint ./second.html:70081221/6688 ./index.html:70081221/1040 ./style.css:70081221/7041 ./script.js:70081221/744

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
-window.onload = function() {
-    var progress = document.getElementById('progress');
-    var width = 0;
-    var interval = setInterval(function() {
-        if (width >= 100) {
-            clearInterval(interval);
-            window.location.href = "second.html"; // Navega a la segunda pantalla
-        } else {
-            width++;
-            progress.style.width = width + '%';
-        }
-    }, 40); // 40ms para completar en 4 segundos
-};
+document.addEventListener('DOMContentLoaded', () => {
+    const progressBar = document.getElementById('progress');
+    progressBar.style.width = '100%';
+
+    // Después de 4 segundos, redirige a la segunda pantalla
+    setTimeout(() => {
+        window.location.href = 'second.html';
+    }, 4000);
+});
